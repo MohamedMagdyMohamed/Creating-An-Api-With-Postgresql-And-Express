4635bf6ae860ee732ccfaf7fe004982a95d99cda
MohamedMagdyMohamed <mm.medany96@gmail.com>
2022-11-12 11:38:25 +0200

    Add Cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from 'express'
+import cors from 'cors';
 import bodyParser from 'body-parser'
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
 
 app.use(bodyParser.json())
+// use cors as a middleware for the whole apis
+app.use(cors())
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
